069d34bda0f6bccbf3d29614f0cdde1e89950c47
kossy-inequality <36091893+kossy-inequality@users.noreply.github.com>
2018-05-07 15:48:10 +0900

    日付の計算を追加

--- mydate.py
+++ mydate.py
@@ -26,3 +26,23 @@ print(todaydetail.microsecond)
 print('----------------------------------')
 print(today.isoformat())
 print(todaydetail.strftime("%Y/%m/%d %H:%M:%S"))
+
+#日付の計算
+today = datetime.datetime.today()
+
+#今日の日付
+print(today)
+
+#明日の日付
+print(today + datetime.timedelta(days=1))
+
+newyear = datetime.datetime(2010,1,1)
+
+#2010/1/1の一週間後
+print(newyear + datetime.timedelta(days=7))
+
+#2010/1/1から今日までの日数
+calc = today - newyear
+
+#計算結果の戻り値は「timedelata」
+print(calc.days)
